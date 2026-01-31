3f5441c67fb7297fe2ee7cfbadfc860547f8ecb0
noppe <noppelabs@gmail.com>
2026-02-01 03:42:14 +0900

    Create Slider.swift

---
+++ Sources/PickerStyles/Slider.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+public struct AdjustmentSlider<Value: BinaryFloatingPoint>: View {
+    let value: Binding<Value>
+    let inRange: ClosedRange<Value>
+    let step: Value
+    
+    public var body: some View {
+        // TODO
+    }
+}
+
