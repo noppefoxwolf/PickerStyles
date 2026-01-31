515bc93e3cba77cd1ca74bfd617653e47c977a0c
noppe <noppelabs@gmail.com>
2026-02-01 01:59:12 +0900

    Update HorizontalScrollPicker.swift

--- Sources/PickerStyles/HorizontalScrollPicker.swift
+++ Sources/PickerStyles/HorizontalScrollPicker.swift
@@ -7,9 +7,6 @@ public struct HorizontalScrollPicker<SelectionValue: Hashable, Content: View>: V
     @Binding
     var selection: SelectionValue
     
-    @State
-    private var centeredTag: SelectionValue?
-    
     public init(
         selection: Binding<SelectionValue>,
         @ViewBuilder content: () -> Content
@@ -19,54 +16,55 @@ public struct HorizontalScrollPicker<SelectionValue: Hashable, Content: View>: V
     }
     
     public var body: some View {
-        ScrollView(.horizontal) {
-            LazyHStack {
-                ForEach(subviews: content) { subview in
-                    if let tag = subview.containerValues.tag(for: SelectionValue.self) {
-                        Button {
-                            selection = tag
-                        } label: {
+        ScrollViewReader { proxy in
+            ScrollView(.horizontal) {
+                LazyHStack {
+                    ForEach(subviews: content) { subview in
+                        if let tag = subview.containerValues.tag(for: SelectionValue.self) {
+                            Button {
+                                selection = tag
+                            } label: {
+                                subview
+                                    .labelStyle(.titleOnly)
+                                    .foregroundStyle(selection == tag ? .black : .secondary)
+                                    .padding(.horizontal, 12)
+                                    .padding(.vertical, 4)
+                                    .background(selection == tag ? .yellow : .clear)
+                                    .mask(Capsule())
+                            }
+                            .foregroundStyle(Color.primary)
+                            .id(tag)
+                        } else {
                             subview
-                                .labelStyle(.titleOnly)
-                                .foregroundStyle(selection == tag ? .black : .secondary)
-                                .padding(.horizontal, 12)
-                                .padding(.vertical, 4)
-                                .background(selection == tag ? .yellow : .clear)
-                                .mask(Capsule())
                         }
-                        .foregroundStyle(Color.primary)
-                        .id(tag)
-                    } else {
-                        subview
                     }
                 }
+                .scrollTargetLayout()
             }
-            .scrollTargetLayout()
-        }
-        .frame(minHeight: 64)
-        .frame(maxWidth: .infinity)
-        .fixedSize(horizontal: false, vertical: true)
-        .scrollTargetBehavior(.viewAligned)
-        .scrollPosition(id: $centeredTag, anchor: .center)
-        .scrollIndicators(.hidden)
-        .mask {
-            LinearGradient(
-                stops: [
-                    Gradient.Stop(color: .clear, location: 0),
-                    Gradient.Stop(color: .black, location: 0.2),
-                    Gradient.Stop(color: .black, location: 0.8),
-                    Gradient.Stop(color: .clear, location: 1),
-                ],
-                startPoint: .leading,
-                endPoint: .trailing
-            )
-        }
-        .onAppear {
-            centeredTag = selection
-        }
-        .onChange(of: selection) { _, newValue in
-            if centeredTag != newValue {
-                centeredTag = newValue
+            .frame(minHeight: 64)
+            .frame(maxWidth: .infinity)
+            .fixedSize(horizontal: false, vertical: true)
+            .scrollTargetBehavior(.viewAligned)
+            .scrollIndicators(.hidden)
+            .mask {
+                LinearGradient(
+                    stops: [
+                        Gradient.Stop(color: .clear, location: 0),
+                        Gradient.Stop(color: .black, location: 0.2),
+                        Gradient.Stop(color: .black, location: 0.8),
+                        Gradient.Stop(color: .clear, location: 1),
+                    ],
+                    startPoint: .leading,
+                    endPoint: .trailing
+                )
+            }
+            .onAppear {
+                proxy.scrollTo(selection, anchor: .center)
+            }
+            .onChange(of: selection) { _, newValue in
+                withAnimation {
+                    proxy.scrollTo(newValue, anchor: .center)
+                }
             }
         }
     }
